cb2fae747b59626dd4565b2e8bc9c483290daf3b
Termtre <fomichiev_2013@mail.ru>
2024-02-18 21:35:16 +0300

    First Version

---
+++ Lab1FDE/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Lab1FDE
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox_y = new System.Windows.Forms.TextBox();
+			this.textBox_x = new System.Windows.Forms.TextBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(8, 532);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(141, 95);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Оси";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(155, 534);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(141, 95);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Синус";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(302, 534);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(141, 95);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Косинус";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(984, 661);
+			this.tabControl1.TabIndex = 4;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.pictureBox1);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.button3);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.tabPage1.Location = new System.Drawing.Point(4, 27);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(976, 630);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Функции синуса и косинуса";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(970, 523);
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.label2);
+			this.tabPage2.Controls.Add(this.label1);
+			this.tabPage2.Controls.Add(this.button4);
+			this.tabPage2.Controls.Add(this.textBox_y);
+			this.tabPage2.Controls.Add(this.textBox_x);
+			this.tabPage2.Controls.Add(this.pictureBox2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 27);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(976, 630);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Плоские фигуры";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.label2.Location = new System.Drawing.Point(6, 595);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(17, 20);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "y";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.label1.Location = new System.Drawing.Point(6, 524);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(17, 20);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "x";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(212, 591);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(102, 31);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Отрисовка";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox_y
+			// 
+			this.textBox_y.Location = new System.Drawing.Point(79, 591);
+			this.textBox_y.Name = "textBox_y";
+			this.textBox_y.Size = new System.Drawing.Size(100, 24);
+			this.textBox_y.TabIndex = 2;
+			// 
+			// textBox_x
+			// 
+			this.textBox_x.Location = new System.Drawing.Point(79, 520);
+			this.textBox_x.Name = "textBox_x";
+			this.textBox_x.Size = new System.Drawing.Size(100, 24);
+			this.textBox_x.TabIndex = 1;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(970, 518);
+			this.pictureBox2.TabIndex = 0;
+			this.pictureBox2.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(984, 661);
+			this.Controls.Add(this.tabControl1);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(1000, 700);
+			this.MinimumSize = new System.Drawing.Size(1000, 700);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox_y;
+		private System.Windows.Forms.TextBox textBox_x;
+	}
+}
+
